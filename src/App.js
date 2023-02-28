1acd0ff6471f4c1989bda46fd2716d1833f2bd93
andrewdelmar87 <andrewjdelmar@gmail.com>
2023-02-27 20:19:22 -0800

    Background color, text color, video modal size

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import IndexPage from './pages/IndexPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import VideoPage from './pages/VideoPage';
+import './pages/indexPage.css';
 
  
 function App() {
