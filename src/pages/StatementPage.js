ecb55223ea017fd53e6e1c6953c2230455c0133e
andrewdelmar87 <andrewjdelmar@gmail.com>
2023-04-10 14:16:23 -0700

    Artist statement & video player size

--- src/pages/StatementPage.js
+++ src/pages/StatementPage.js
@@ -18,9 +18,16 @@ return (
               <div className="video_body">
                   <h1 className="text-center mb-5">BOATS I HAVE SEEN</h1>
                   <div className="video_list">  
-                  <div className="video_item" style={{padding: "0 5% 0 5%", color: 'white'}}> 
+                  <div className="video_item" style={{padding: "0 13% 0 13%", color: 'white'}}> 
                      <h3>Artist Statement</h3>
-                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas tempora iure placeat suscipit recusandae deleniti nesciunt repellendus sapiente deserunt architecto? Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quas repudiandae adipisci aliquam deleniti voluptatibus at, sint soluta sunt animi eveniet nam. Voluptatibus vel animi ab modi nostrum doloribus exercitationem!</p>
+                     <p>Boats I Have Seen (BIHS) is a collection of very short videos intended as meditations and escapism when either are needed with little time to spare.<br/><br/>
+                        Made during a months-long visit through a winter in Seattle WA while living on a small houseboat on Lake Union, each video was filmed using a handheld iPhone X.<br/><br/>
+                        A friend visited the houseboat and said I would need to write a novel while living there. I answered, writing a single page is torture enough. But the truth is he kindled a responsibility to create some aesthetic response of the experience. BIHS is it.<br/><br/>
+                        The videos are unedited except for the front title and the fades.  They do not catalog the large numbers and types of boats to be seen in and around Seattle and its remarkable bodies of water. Their intentions are specific to the focus of stillness and movement. Each was made in place with that same reflection.<br/><br/>
+                        You may find yourself here because you just like boats. That’s fine, too.<br/><br/>
+                        Joshua Wattles<br/>
+                        joshuawattles@gmail.com<br/>
+                        4/10/23</p>
                   </div>                    
                   </div>
               </div>
